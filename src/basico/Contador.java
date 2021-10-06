90b924d8c675fe7274ba53666df817b1efb3cb2e
Lucas Severo <lucasleite_14.1@hotmail.com>
2021-10-06 15:57:27 -0300

    Contador #3 - Alterando Layout com CSS

--- src/basico/Contador.java
+++ src/basico/Contador.java
@@ -17,7 +17,11 @@ public class Contador extends Application{
 	public void start(Stage primaryStage) throws Exception {
 		
 		Label labelTitulo = new Label("Contador");
+		labelTitulo.getStyleClass().add("titulo");
+		
 		Label labelNumero = new Label("0");
+		labelNumero.getStyleClass().add("numero");
+		
 		
 		Button botaoDecremento = new Button("-");
 		botaoDecremento.setOnAction(e -> {
@@ -38,15 +42,20 @@ public class Contador extends Application{
 		boxBotoes.getChildren().add(botaoIncremento);
 		
 		
-		VBox boxPrincipal = new VBox();
-		boxPrincipal.setSpacing(10);
-		boxPrincipal.setAlignment(Pos.CENTER);
-		boxPrincipal.getChildren().add(labelTitulo);
-		boxPrincipal.getChildren().add(labelNumero);
-		boxPrincipal.getChildren().add(boxBotoes);
+		VBox boxConteudo = new VBox();
+		boxConteudo.getStyleClass().add("conteudo");
+		boxConteudo.setSpacing(10);
+		boxConteudo.setAlignment(Pos.CENTER);
+		boxConteudo.getChildren().add(labelTitulo);
+		boxConteudo.getChildren().add(labelNumero);
+		boxConteudo.getChildren().add(boxBotoes);
 		
+		String caminhoDoCss = getClass()
+				.getResource("/basico/Contador.css").toExternalForm();
 		
-		Scene cenaPrincipal = new Scene(boxPrincipal, 400, 400);
+		Scene cenaPrincipal = new Scene(boxConteudo, 400, 400);
+		cenaPrincipal.getStylesheets().add(caminhoDoCss);
+		cenaPrincipal.getStylesheets().add("https://fonts.googleapis.com/css?family=Oswald");
 		
 		primaryStage.setScene(cenaPrincipal);
 		primaryStage.show();
