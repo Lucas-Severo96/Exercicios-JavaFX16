f3bcd3549a07ccc94a5c4b4e424ca7b67ce4060c
Lucas Severo <lucasleite_14.1@hotmail.com>
2021-10-14 19:11:34 -0300

    StackPane - Parte 02

--- src/layout/TesteStackPane.java
+++ src/layout/TesteStackPane.java
@@ -1,5 +1,6 @@
 package layout;
 
+import javafx.application.Platform;
 import javafx.scene.layout.StackPane;
 
 public class TesteStackPane extends StackPane {
@@ -13,7 +14,7 @@ public class TesteStackPane extends StackPane {
 		Caixa c5 = new Caixa().comTexto("5");
 		Caixa c6 = new Caixa().comTexto("6");
 		
-		getChildren().addAll(c1, c2, c3, c4, c5, c6);
+		getChildren().addAll(c2, c3, c4, c5, c6, c1);
 		
 		setOnMouseClicked(e -> {
 			if(e.getSceneX() > getScene().getWidth()/ 2) {
@@ -22,5 +23,22 @@ public class TesteStackPane extends StackPane {
 				getChildren().get(5).toBack();
 			}
 		});
+		
+		Thread t = new Thread(() -> {
+			while (true) {
+				try {
+					Thread.sleep(3000);
+					
+					Platform.runLater(() -> {
+						getChildren().get(0).toFront();
+					});
+				} catch (Exception e) {
+					
+				}
+			}
+		});
+		
+		t.setDaemon(true);
+		t.start();
 	}
 }
